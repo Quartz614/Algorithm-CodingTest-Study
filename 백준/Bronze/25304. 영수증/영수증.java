ebd115388605c2234a841496883847c17afa4737
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2022-08-23 23:04:27 +0900

    [Bronze V] Title: 영수증, Time: 124 ms, Memory: 13104 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int total = sc.nextInt();
+        int cnt = sc.nextInt();
+        int sum = 0;
+        for(int i = 0; i < cnt; i++) {
+            int price = sc.nextInt();
+            int num = sc.nextInt();
+            sum += price * num;
+        }
+        if (total == sum) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
