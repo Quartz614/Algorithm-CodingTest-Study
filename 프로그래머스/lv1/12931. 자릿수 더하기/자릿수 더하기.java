b4af1ae9afc1fe690cdf61e4f8469cbd02ef6c62
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2022-12-19 10:39:21 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -1,13 +1,10 @@
-import java.util.*;
-
 public class Solution {
     public int solution(int n) {
-        int answer = 0;
-        
-        while(n > 0) {
-            answer += n % 10;
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
             n /= 10;
         }
-        return answer;
+        return sum;
     }
 }
