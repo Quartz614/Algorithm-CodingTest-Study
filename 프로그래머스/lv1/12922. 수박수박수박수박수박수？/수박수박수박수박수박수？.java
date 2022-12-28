0c4c893cb4fd0acf4015a9db1863c82f8039ae68
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2022-12-28 10:48:03 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 2.72 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,9 +1,12 @@
 class Solution {
     public String solution(int n) {
         String answer = "";
-        for(int i = 1; i<=n; i++){
-            if(i % 2 != 0) answer += "수";
-            else answer += "박";
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                answer += "수";
+            } else {
+                answer += "박";
+            }
         }
         return answer;
     }
