3299ae9373a68277a86cc8cebb59d6a9965f9f4e
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2023-01-25 12:13:23 +0900

    [Bronze V] Title: A+B, Time: 112 ms, Memory: 12860 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,15 +1,12 @@
 import java.util.Scanner;
- 
+
 public class Main {
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
-		int A = in.nextInt();
-		int B = in.nextInt();
-		
-		System.out.println(A+B);
- 
-		in.close();
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A + B);
+    }
 }
