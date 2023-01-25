cc6b81629a2f2fed8e49ff9aaf4d17e28f4190e6
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2023-01-25 12:23:52 +0900

    [Bronze V] Title: 사칙연산, Time: 112 ms, Memory: 12964 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -1,7 +1,9 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int A = sc.nextInt();
         int B = sc.nextInt();
 
@@ -10,6 +12,5 @@ public class Main {
         System.out.println(A * B);
         System.out.println(A / B);
         System.out.println(A % B);
-        sc.close();
     }
 }
