8cfb4c9901831e4cfb368a8d97640e835c14f942
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2023-01-25 12:53:11 +0900

    [Bronze V] Title: 시험 성적, Time: 80 ms, Memory: 11472 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,18 +1,19 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       int a = sc.nextInt();
-       if(a >= 90 && a <= 100) {
-           System.out.println("A");
-       }else if(a >= 80 && a <=89) {
-           System.out.println("B");
-       }else if(a >= 70 && a <= 79) {
-           System.out.println("C");
-       }else if(a >= 60 && a <= 69) {
-           System.out.println("D");
-       }else {
-           System.out.println("F");
-       }
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        int A = Integer.parseInt(str);
+        if (A >= 90 && A <= 100) System.out.println("A");
+        else if (A >= 80 && A <= 89) System.out.println("B");
+        else if (A >= 70 && A <= 79) System.out.println("C");
+        else if (A >= 60 && A <= 69) System.out.println("D");
+        else System.out.println("F");
     }
 }
