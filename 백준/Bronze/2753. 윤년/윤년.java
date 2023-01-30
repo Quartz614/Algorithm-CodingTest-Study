3177881eb81ecc6957226d4228ec8efc70eb8a7c
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2023-01-30 11:25:28 +0900

    [Bronze V] Title: 윤년, Time: 112 ms, Memory: 12848 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,12 +1,17 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       int a = sc.nextInt();
-       if(a % 4 == 0 && a % 100 != 0 || a % 400 == 0) {
-           System.out.println("1");
-       }else {
-           System.out.println("0");
-       }
+        Scanner in = new Scanner(System.in);
+
+        int y = in.nextInt();
+        in.close();
+
+        if(y%4==0) {
+            if(y%400==0) System.out.println("1");
+            else if(y%100==0) System.out.println("0");
+            else System.out.println("1");
+        }
+        else System.out.println("0");
     }
 }
