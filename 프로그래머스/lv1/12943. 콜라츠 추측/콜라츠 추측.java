c9f10deb6c57c681abd2fab31f2c840504534bb7
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2023-02-04 01:17:49 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,18 +1,17 @@
 class Solution {
-    public int solution(long num) {
-        int answer = 0;
-        while (num != 1) {
-            if (num % 2 == 0) {
-                num /= 2;
-                answer++;
-            } else if (num % 2 != 0) {
-                num = num * 3 + 1;
-                answer++;
+    public int solution(int num) {
+        long n = num;
+        int cnt = 0;
+        
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = n * 3 + 1;
             }
+            cnt++;
+            if (cnt > 500) return -1;
         }
-        if (answer > 500) {
-            answer = -1;
-        }
-        return answer;
+        return cnt;
     }
 }
