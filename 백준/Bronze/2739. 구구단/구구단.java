c26ee0217d72c4397c41d5fa1473d6f213fb6c75
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2023-02-03 11:07:35 +0900

    [Bronze V] Title: 구구단, Time: 88 ms, Memory: 11444 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,13 +1,15 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-
-        for (int i = 1; i <= 9; i++) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        for (int i = 1; i < 10; i++) {
             System.out.println(a + " * " + i + " = " + a * i);
+            }
         }
-    }
 }
-
