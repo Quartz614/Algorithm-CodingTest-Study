d3acd1229f6f157998c5457febf44214709b4722
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2023-02-04 00:32:22 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -2,11 +2,13 @@ class Solution {
     public boolean solution(int x) {
         int sum = 0;
         int a = x;
-        while (a > 0) {
+        
+        while (a >= 1) {
             sum += a % 10;
             a /= 10;
         }
-        if (x % sum == 0) return true;
-        else return false;
+        if (x % sum == 0) {
+            return true;
+        } else return false;
     }
 }
