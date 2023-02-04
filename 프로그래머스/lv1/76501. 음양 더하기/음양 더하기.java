10088105db2e04407820a6e1e673e01d2aa38e40
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2023-02-04 21:02:08 +0900

    [level 1] Title: 음양 더하기, Time: 0.05 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -1,8 +1,7 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
-        
-        for (int i = 0; i < signs.length; i++) {
+        for (int i = 0; i < absolutes.length; i++) {
             if (signs[i]) {
                 answer += absolutes[i];
             } else {
