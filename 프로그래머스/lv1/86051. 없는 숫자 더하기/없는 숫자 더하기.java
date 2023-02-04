730285a15cd6511364df3a84b7769352f543aff3
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2023-02-04 21:18:31 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,10 +1,9 @@
 class Solution {
     public int solution(int[] numbers) {
-        int sum = 45;
-        
-        for (int i = 0; i < numbers.length; i++) {
-            sum -= numbers[i];
+        int answer = 45;
+        for (int n : numbers) {
+            answer -= n;
         }
-        return sum;
+        return answer;
     }
 }
