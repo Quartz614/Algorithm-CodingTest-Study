5b449196df4e3d3a2589a951fe6b88bc135acd42
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2023-02-27 13:10:12 +0900

    [Bronze V] Title: 아스키 코드, Time: 80 ms, Memory: 11460 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -1,10 +1,12 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.next();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
         char c = s.charAt(0);
-        System.out.println(c + 0);
+        System.out.print((int)c);
     }
 }
