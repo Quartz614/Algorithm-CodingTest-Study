70244fbba8d17b60b6498cbf1011f10452eeb637
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2023-04-03 23:47:49 +0900

    [Bronze I] Title: 단어 공부, Time: 176 ms, Memory: 20508 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,23 +1,31 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[26];
+        String str = br.readLine();
+        for (int i = 0; i < str.length(); i++) {
+            int str_char = str.charAt(i);
+            if (97 <= str_char && str_char <= 122) {
+                arr[str_char - 97]++;
+            } else {
+                arr[str_char - 65]++;
+            }
+        }
+        int max = -1;
+        char idx = 0;
 
-    public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       int [] arr = new int[26];
-
-       String a = sc.next().toUpperCase();
-
-       int max = -1;
-       char st = '?';
-
-       for (int i = 0; i < a.length(); i++) {
-           arr[a.charAt(i) - 65]++;
-           if(max < arr[a.charAt(i) - 65]) {
-               max = arr[a.charAt(i) -65];
-               st = a.charAt(i);
-           }
-           else if(max == arr[a.charAt(i) - 65]) st = '?';
-       }
-        System.out.println(st);
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                idx = (char) (i + 65);
+            } else if (arr[i] == max) {
+                idx = '?';
+            }
+        }
+        System.out.println(idx);
     }
 }
