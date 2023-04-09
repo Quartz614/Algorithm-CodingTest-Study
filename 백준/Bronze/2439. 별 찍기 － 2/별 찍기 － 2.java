05844da3c9a17543c40330674bd278967045dc4a
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2023-04-09 20:40:42 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 216 ms, Memory: 13876 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,19 +1,21 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        br.close();
 
-        for (int i = 1; i <= a; i++) {
-            for (int j = 1; j <= a - i; j++) {
+        for( int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N - i; j++) {
                 System.out.print(" ");
             }
-            for(int j = 1; j <= i; j++) {
+            for (int k = 1; k <= i; k++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
 }
-
