4282ef0bac9788425ffb91cc65d0d0327bebddf7
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2023-04-10 22:56:23 +0900

    [Bronze III] Title: 최댓값, Time: 80 ms, Memory: 11480 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,22 +1,29 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int[] arr = new int[9];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
+
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
+
         int max = 0;
-        int maxIndex = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-                maxIndex = i + 1;
+        int index = 0;
+        int cnt = 0;
+
+        for (int value : arr) {
+            cnt++;
+            if (value > max) {
+                max = value;
+                index = cnt;
             }
         }
         System.out.println(max);
-        System.out.println(maxIndex);
-
+        System.out.println(index);
     }
 }
