74189ba397bd51fd5a9f645fc115c94311ea1260
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2023-04-25 22:20:20 +0900

    [Bronze II] Title: 문자열 반복, Time: 76 ms, Memory: 11448 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,21 +1,33 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int t = Integer.parseInt(br.readLine()); // 테스트 케이스의 개수
 
-        for (int i = 0; i < n; i++) {
-            int num = sc.nextInt();
-            String str = sc.next();
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-            for (int j = 0; j < str.length(); j++) {
-                for (int k = 0; k < num; k++) {
-                    System.out.print(str.charAt(j));
+            int r = Integer.parseInt(st.nextToken()); // 반복 횟수
+            String s = st.nextToken(); // 문자열 S
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int j = 0; j < s.length(); j++) {
+                char c = s.charAt(j);
+
+                for (int k = 0; k < r; k++) {
+                    sb.append(c);
                 }
             }
-            System.out.println();
+
+            System.out.println(sb.toString()); // 출력
         }
-        sc.close();
+
+        br.close();
     }
 }
