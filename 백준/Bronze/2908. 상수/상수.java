8af45536d3cc9473b10e761513fafec3b3251fe3
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2023-05-01 23:57:10 +0900

    [Bronze II] Title: 상수, Time: 76 ms, Memory: 11484 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,27 +1,19 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
+    public static void main(String[] args) throws IOException, IOException {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int reverse_a = 0;
-        int reverse_b = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        while (a != 0) {
-            int digit = a % 10;
-            reverse_a = reverse_a * 10 + digit;
-            a /= 10;
-        }
-        while (b != 0) {
-            int digit = b % 10;
-            reverse_b = reverse_b * 10 + digit;
-            b /= 10;
-        }
-        if (reverse_a > reverse_b) {
-            System.out.println(reverse_a);
-        } else {
-            System.out.println(reverse_b);
-        }
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+
+        System.out.print(A > B ? A:B);
+
     }
 }
