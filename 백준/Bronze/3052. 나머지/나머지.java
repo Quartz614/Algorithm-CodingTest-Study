29284a1f54a995db9c16b73c37f97a46a6f7b30e
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2023-05-02 22:49:17 +0900

    [Bronze II] Title: 나머지, Time: 76 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,31 +1,31 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NumberFormatException, IOException {
 
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int[] arr = new int[10];
-        int temp = 0;
-        int count = 0;
+        boolean bl;
+        int cnt = 0;
 
-        for (int i = 0; i < 10; i++) {
-            arr[i] = scanner.nextInt() % 42;
+        for(int i=0; i<arr.length; i++ ) {
+            arr[i] = Integer.parseInt(br.readLine()) % 42;
         }
-
-        for (int i = 0; i < 10; i++) {
-            temp = 0;
-            for (int k = i + 1; k < 10; k++) {
-                if (arr[i] == arr[k]) {
-                    temp++;
+        for(int i=0; i<10; i++ ) {
+            bl = false; 
+            for(int k=i+1; k<arr.length; k++ ) {
+                if( arr[i] == arr[k] ) {
+                    bl = true;
+                    break;
                 }
             }
-            if (temp == 0) {
-                count++;
+            if( bl == false ) {
+                cnt++;
             }
         }
-
-        System.out.println(count);
-
+        System.out.println(cnt);
     }
 }
