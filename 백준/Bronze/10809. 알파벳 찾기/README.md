5a5a3226d47e467666fbc71ed2e51109fef7742b
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2023-05-05 23:58:14 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 80 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/README.md
+++ 백준/Bronze/10809. 알파벳 찾기/README.md
@@ -1,14 +1,14 @@
-# [Bronze V] 알파벳 찾기 - 10809 
+# [Bronze II] 알파벳 찾기 - 10809 
 
 [문제 링크](https://www.acmicpc.net/problem/10809) 
 
 ### 성능 요약
 
-메모리: 17664 KB, 시간: 212 ms
+메모리: 11516 KB, 시간: 80 ms
 
 ### 분류
 
-구현(implementation), 문자열(string)
+구현, 문자열
 
 ### 문제 설명
 
--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,13 +1,29 @@
-import java.util.*;
- 
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
-        String s = sc.nextLine();
-        
-        for(char i = 'a'; i <= 'z'; i++){
-            System.out.println(s.indexOf(i));
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[26];
+
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = -1;
+        }
+
+        String S = br.readLine();
+
+        for(int i = 0; i < S.length(); i++) {
+            char ch = S.charAt(i);
+
+            if(arr[ch - 'a'] == -1) {
+                arr[ch - 'a'] = i;
+            }
+        }
+
+        for(int val : arr) {
+            System.out.print(val + " ");
         }
     }
 }
